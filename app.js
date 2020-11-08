b95f615285979fbf0c921c4e38c2ff70ea449e0e
Grafista.pe <hola@grafista.pe>
2020-11-08 15:50:57 -0500

    node js server started manualy

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit(); detiene el servidor al instante
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<h1>Hello from NodeJS</h1>');    
+    res.write('</html>');
+});
+
+server.listen(3000);
